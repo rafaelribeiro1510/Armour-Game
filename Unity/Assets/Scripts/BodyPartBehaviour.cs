94c29191dd7a3ef9adf39118c8011307a7507eea
up201806330 <45789887+up201806330@users.noreply.github.com>
2021-03-05 23:02:47 +0000

    Imported DOTween ; Basic drag and dropping on target

---
+++ Unity/Assets/Scripts/BodyPartBehaviour.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BodyPartBehaviour : MonoBehaviour
+{
+    public enum Type{
+        Head,
+        Torso,
+        LArm,
+        RArm,
+        LLeg,
+        RLeg
+    }
+    
+    [SerializeField]
+    Type BodyType = Type.Torso;
+
+    [Header("Return parameters")]
+    [SerializeField]
+    float returnDuration;
+    [SerializeField]
+    Ease returnEase = Ease.Linear;
+    
+
+    bool IsGrabbed { get; set; }
+    [SerializeField]
+    bool wrongPosition = true;
+    
+    Collider2D col;
+    Vector3 initialPos;
+
+    TargetBehaviour currHovering;
+    
+    void Awake() {
+        col = GetComponent<Collider2D>();    
+        initialPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0){
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if (touch.phase == TouchPhase.Began){
+                Collider2D touchedCollider = Physics2D.OverlapPoint(touchPos);
+                if (col == touchedCollider){
+                    IsGrabbed = true;
+                }
+            }
+
+            else if (touch.phase == TouchPhase.Moved && IsGrabbed){
+                transform.position = touchPos;
+            }
+
+            else if (touch.phase == TouchPhase.Ended){
+                IsGrabbed = false;
+                if (transform.position != initialPos){
+                    if (wrongPosition)  
+                        transform.DOMove(initialPos, returnDuration).SetEase(returnEase);
+                    else{
+                        transform.DOMove(currHovering.transform.position, returnDuration/2).SetEase(returnEase);
+                        currHovering.stopGlowing();
+                    }
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Target")){
+            if (currHovering == null) currHovering = other.GetComponent<TargetBehaviour>();
+            else print("Uhhh currHovering aint null cuh");
+
+            if (currHovering.BodyType == this.BodyType){
+                wrongPosition = false;
+                currHovering.startGlowing();
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("Target")){
+            wrongPosition = true;
+            currHovering.stopGlowing();
+            currHovering = null;
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/TargetBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using DG.Tweening;
+
+public class TargetBehaviour : MonoBehaviour
+{
+    public BodyPartBehaviour.Type BodyType = BodyPartBehaviour.Type.Torso;
+
+    SpriteRenderer sprite;
+    bool glowing = false;
+    Color ogColor;
+
+    [SerializeField]
+    float blinkDuration;
+    Sequence blinkTween;
+
+    void StartBlink() {
+        blinkTween = DOTween.Sequence();
+        blinkTween.Append(sprite.DOColor(new Color(1,1,1,0.5f), blinkDuration)).Append(sprite.DOColor(ogColor, blinkDuration)).SetLoops(Int32.MaxValue);
+    }
+
+    private void Awake() {
+        sprite = GetComponent<SpriteRenderer>();
+        ogColor = sprite.color;
+    }
+
+    public void startGlowing(){
+        if (glowing) return;
+        StartBlink();
+    }
+
+    public void stopGlowing(){
+        if (!glowing) return;
+        blinkTween.Kill(true);
+    }
+
+}
